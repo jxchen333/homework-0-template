f5d995fed64b59aa794607a8fd9d81a67837d714
jxchen333 <jxchen333@uchicago.edu>
2021-10-02 22:19:25 +0800

    Q2
    majority is finished

--- egyptian.py
+++ egyptian.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     for a in [1,2,3]:
         for n in [1,2,5,10]:
             print("{} * {} = {}".format(a, n, egyptian_multiplication(a,n)))
+            print("{} ^ {} = {}".format(a, n, egyptian_multiplication(a,n)))
 
 
 def power(a, n):
@@ -38,4 +39,26 @@ def power(a, n):
 
     assume n is a nonegative integer
     """
-    pass
+    # copy the code from the template, since the idea is similar
+    def isodd(n):
+        """
+        returns True if n is odd
+        """
+        return n & 0x1 == 1
+    
+    if n == 1:
+        return a # a^1=a
+    if n == 0:
+        return 1 # a^0=1
+
+    if isodd(n):
+        return power(a * a, n // 2) * a # a^(2k+1) = (a^2)^k * a
+    else:
+        return power(a * a, n // 2) # a^(2k) = (a^2)^k
+
+if __name__ == '__main__':
+    # this code runs when executed as a script
+    print('------------POWER-------------------------------')
+    for a in [1,2,3]:
+        for n in [1,2,5,10]:
+            print("{} ^ {} = {}".format(a, n, power(a,n)))
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,4 +7,13 @@ for multiples of 5 print "buzz" instead of the number,
 and for multiples of both 3 and 5 print "fizzbuzz" instead of the number.
 """
 
-print(i)
+for i in range(100):
+    j = i + 1 # 1~100
+    if j%15 == 0:
+        print('fizzbuzz')
+    elif j%3 == 0:
+        print('fizz')
+    elif j%5 == 0:
+        print('buzz')
+    else:
+        print(j)
